412bb05c3831494a50dab9a7d1f51a2302fdd205
KilianCO <123088485+KilianCO@users.noreply.github.com>
2023-10-12 14:02:50 +0200

    Update and rename TP3_RegQuantile_suite.R to RegLin_TP3_RegQuantile_suite.R

--- RegLin_TP3_RegQuantile_suite.R
+++ RegLin_TP3_RegQuantile_suite.R
@@ -15,17 +15,17 @@ vituse=factor(vituse)
 Y=log(betaplasma+0.1)
 shapiro.test(Y);hist(Y);boxplot(Y)
 
-# modèle sur log(betaplasma+0.1)
+# modÃ¨le sur log(betaplasma+0.1)
 m1_LS=lm(Y~age+sex+smokstat+quetelet+vituse+calories+fat+fiber+alcohol+cholest+betadiet+retdiet+retplasma)
 coef_LS=coef(m1_LS)
 shapiro.test(x=residuals(m1_LS))
 hist(residuals(m1_LS));boxplot(residuals(m1_LS))
 summary(m1_LS)
 
-# modèle sur betaplasma
+# modÃ¨le sur betaplasma
 m1_Q1=rq(Y~age+sex+smokstat+quetelet+vituse+calories+fat+fiber+alcohol+cholest+betadiet+retdiet+retplasma)
 summary(m1_Q1) # affiche seulement les intervalles de confiance des estimateurs
-summary(m1_Q1,se="iid") # affiche les résultats des tests d'hypothèse
+summary(m1_Q1,se="iid") # affiche les rÃ©sultats des tests d'hypothÃ¨se
 coef_Q1=coef(m1_Q1) #les coeffs
 shapiro.test(x=residuals(m1_Q1))
 hist(residuals(m1_Q1));boxplot(residuals(m1_Q1))
