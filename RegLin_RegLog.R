c350670f06a5ba28b856adc124629897cf91fe4c
KilianCO <123088485+KilianCO@users.noreply.github.com>
2023-10-12 14:02:05 +0200

    Update and rename RegLog.R to RegLin_RegLog.R

--- RegLin_RegLog.R
+++ RegLin_RegLog.R
@@ -16,26 +16,26 @@ summary(m2)
 # Exercice 1 ####
 
 
-# Exercice ‡ rÈaliser en TP ####
+# Exercice √† r√©aliser en TP ####
 exoTP=read.table('exo_TP_regr_logistic.txt',col.names=c("remiss","cell","li","blast"))
 attach(exoTP)
 
 cat("\n QUESTION 1 \n")
 table(remiss)
-# Y=1 rÈmission // Y=0 non-rÈmission
-# Seulement 9 rÈmissions et 18 non rÈmissions ==> n=27
+# Y=1 r√©mission // Y=0 non-r√©mission
+# Seulement 9 r√©missions et 18 non r√©missions ==> n=27
 # P[Y=1]=9/27=1/3
 
 cat("\n QUESTION 2 \n")
-# On modÈlise P[Y=1|X=x]=pi(x)
+# On mod√©lise P[Y=1|X=x]=pi(x)
 # Avec X=(X_1,X_2,X_3) le vecteur des variables explicatives (cell,li,blast)
-# Ces variables sont toutes numÈriques
+# Ces variables sont toutes num√©riques
 # Et x=(x1,x2,x3) une mesure
 
-# D'aprËs le cours :
+# D'apr√®s le cours :
 # pi(x)=exp(b0+b1*x1+b2*x2+b3*x3)/(1+exp(b0+b1*x1+b2*x2+b3*x3))
 
-# ModËle logistique => estimer b0,b1,b2,b3
+# Mod√®le logistique => estimer b0,b1,b2,b3
 
 m2 = glm(remiss ~ cell+li+blast, family="binomial")
 #On indique que Y suit une loi binomiale
@@ -45,30 +45,30 @@ summary(m2)
 
 # Ho : la variable "cell" n'influe pas sur P[Y=1|X2,X3]
 # b1=0 si X2,X3 sont pris en compte en P[Y=1]
-# ModËle : pi(x2,x3)=exp(b0+b2*x2+b3*x3)/(1+exp(b0+b2*x2+b3*x3))
+# Mod√®le : pi(x2,x3)=exp(b0+b2*x2+b3*x3)/(1+exp(b0+b2*x2+b3*x3))
 
 # H1 : X1 influe P[Y=1|X2,X3]
 # b1 =/= 0 si X2,X3 sont pris en compte en P[Y=1]
 # => (M2)
 
-# z=1.058 et pvalue=0.29 (supÈrieur ‡ 0.05) ==> HO acceptÈe
+# z=1.058 et pvalue=0.29 (sup√©rieur √† 0.05) ==> HO accept√©e
 
-# b0=0, b1=0, b3=0 acceptÈes
+# b0=0, b1=0, b3=0 accept√©es
 # Mais X2 influe P[Y=1]
 
 cat("\n QUESTION 4 \n")
 # b0hat = -10.6
 # Si x1=x2=x3=0 alors P[Y=1]=exp(-10.6)/(1+exp(-10.6))
-# Mais puisque l'hypothËse b0=0 est acceptÈe, on a aussi :
+# Mais puisque l'hypoth√®se b0=0 est accept√©e, on a aussi :
 # P[Y=1]=exp(0)/(1+exp(0))=1/2
 
 # b1hat = 7.46 > 0 
 # Si X1 augmente alors P[Y=1|X1,X2,X3] augmente aussi.
-# Mais cette augmentation n'est pas significative car H0 : b1=0 est acceptÈe
+# Mais cette augmentation n'est pas significative car H0 : b1=0 est accept√©e
 
 # b2hat = 3.26
 # Si X2 augmente alors P[Y=1|X1,X2,X3] augmente aussi
-# De maniËre significative car b2 =/= 0
+# De mani√®re significative car b2 =/= 0
 # pi(x1,x2+1,x3)= ...
 # pi(x1,x2,x3)= ...
 # pi[x1,x2+1,x3]/pi[x1,x2,x3] = exp(b2)/(1+exp(b0+b1*x1+b2(x2+1)+b3*x3))
@@ -76,13 +76,13 @@ cat("\n QUESTION 4 \n")
 cat("\n QUESTION 5 \n")
 pi2=predict(m2,type="response")
 prev2=vector(mode="numeric",length=length(pi2))
-prev2[pi2>.5]=1  ## prevision de remiss par lemodËle m2
+prev2[pi2>.5]=1  ## prevision de remiss par lemod√®le m2
 pred2=predict(m2,type="response")
 print(pred2)
 table(remiss,prev2)
 
 # Le taux de mauvais classement pour la non-remission est 2/18
-# Le taux de mauvais classement pour la rÈmission est de 5/9
+# Le taux de mauvais classement pour la r√©mission est de 5/9
 
 cat("\n QUESTION 6 \n")
 m3=glm(remiss~li,family="binomial")
@@ -90,7 +90,7 @@ m3=glm(remiss~li,family="binomial")
 cat("\n QUESTION 7 \n")
 pi3=predict(m3,type="response")
 prev3=vector(mode="numeric",length=length(pi3))
-prev3[pi3>.5]=1  ## prevision de remiss par lemodËle m2
+prev3[pi3>.5]=1  ## prevision de remiss par lemod√®le m2
 
 table(remiss,prev3)
 
